81ef901180060ea7c461bb653cce63f850ffa6b9
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-02 15:22:08 +0500

    Update main.js
    Signed-off-by: Richardson736 <162537734+richardgrechko@users.noreply.github.com>

--- js/main/main.js
+++ js/main/main.js
@@ -5,3 +5,50 @@ var app = new Vue({
     methods: functions,
     created: onCreate
 });
+function getSaveCode() {
+    return btoa(unescape(encodeURIComponent(JSON.stringify(tmp))));
+}
+function saveGame(){
+    let str = getSaveCode();
+    localStorage.setItem("NumberMultiplierGameSave", str);
+}
+function loadGame(importString) {
+    let loadVal = function(v, alt) {
+        return v !== undefined ? v : alt;
+    }
+
+    let item = importString !== undefined ? importString : localStorage.getItem("NumberMultiplierGameSave");
+    if(item !== null)
+    {
+        let obj;
+        try
+        {
+            obj = JSON.parse(decodeURIComponent(escape(atob(item))));
+        }
+        catch(e)
+        {
+            alert("Error loading Game: " + e);
+            return;
+        }
+        game.rhoParticles = loadVal(E(obj.rhoParticles), E(0));
+        game.thetaEnergy = loadVal(E(obj.thetaEnergy), E(0));
+    }
+}
+function onCreate()
+{
+    initialGame = getSaveCode();
+
+    loadGame();
+
+    requestAnimationFrame(update);
+}
+function update()
+{
+    dt2 = Date.now();
+    let dt = (dt2 - dt1) / 1000;
+    dt1 = Date.now();
+
+    tmp.number = tmp.number.mul(tmp.multiplier.pow(1/dt));
+
+    requestAnimationFrame(update);
+}
