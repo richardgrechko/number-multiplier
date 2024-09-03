8cc650f3928dd7bf20cc36b43cbfc5bca8e8e419
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-03 13:44:42 +0500

    Update number.js
    Signed-off-by: Richardson736 <162537734+richardgrechko@users.noreply.github.com>

--- js/main/number.js
+++ js/main/number.js
@@ -3,9 +3,5 @@ var app = new Vue({
     el: "#app",
     data: tmp,
 });
-tmp.FPS = E(60);
 tmp.number = E(1);
 tmp.multiplier = E(2);
-setInterval(()=>{
-  tmp.number = tmp.number.mul(tmp.multiplier.pow(1/tmp.FPS));
-}, 1000/tmp.FPS);
