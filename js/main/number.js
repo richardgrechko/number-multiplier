41447cf2b1d453ee235294c995fe5a27f0c5a8d5
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-03 13:44:34 +0500

    Update number.js
    Signed-off-by: Richardson736 <162537734+richardgrechko@users.noreply.github.com>

--- js/main/number.js
+++ js/main/number.js
@@ -2,9 +2,6 @@ let tmp = {};
 var app = new Vue({
     el: "#app",
     data: tmp,
-    computed: computed,
-    methods: functions,
-    created: onCreate
 });
 tmp.FPS = E(60);
 tmp.number = E(1);
