dfe30da984d58e924d80083f46bd7859f7261418
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-03 13:50:08 +0500

    Update main.js
    Signed-off-by: Richardson736 <162537734+richardgrechko@users.noreply.github.com>

--- js/main/main.js
+++ js/main/main.js
@@ -1,47 +1,49 @@
-function getSaveCode() {
-    return btoa(unescape(encodeURIComponent(JSON.stringify(tmp))));
-}
-function saveGame(){
-    let str = getSaveCode();
-    localStorage.setItem("NumberMultiplierGameSave", str);
-}
-function loadGame(importString) {
-    let loadVal = function(v, alt) {
-        return v !== undefined ? v : alt;
-    }
+let functions = {
+    getSaveCode: function() {
+        return btoa(unescape(encodeURIComponent(JSON.stringify(tmp))));
+    },
+    saveGame: function(){
+        let str = getSaveCode();
+        localStorage.setItem("NumberMultiplierGameSave", str);
+    },
+    loadGame: function(importString) {
+        let loadVal = function(v, alt) {
+            return v !== undefined ? v : alt;
+        }
 
-    let item = importString !== undefined ? importString : localStorage.getItem("NumberMultiplierGameSave");
-    if(item !== null)
-    {
-        let obj;
-        try
+        let item = importString !== undefined ? importString : localStorage.getItem("NumberMultiplierGameSave");
+        if(item !== null)
+        {
+            let obj;
+            try
         {
             obj = JSON.parse(decodeURIComponent(escape(atob(item))));
         }
-        catch(e)
+            catch(e)
         {
             alert("Error loading Game: " + e);
             return;
         }
-        game.number = loadVal(E(obj.number), E(0));
-        game.multiplier = loadVal(E(obj.multiplier), E(0));
+            tmp.number = loadVal(E(obj.number), E(0));
+            tmp.multiplier = loadVal(E(obj.multiplier), E(0));
+        }
+    },
+    onCreate: function()
+    {
+        initialGame = getSaveCode();
+
+        loadGame();
+
+        requestAnimationFrame(update);
+    },
+    update: function()
+    {
+        dt2 = Date.now();
+        let dt = (dt2 - dt1) / 1000;
+        dt1 = Date.now();
+
+        tmp.number = tmp.number.mul(tmp.multiplier.pow(1/dt));
+
+        requestAnimationFrame(update);
     }
-}
-function onCreate()
-{
-    initialGame = getSaveCode();
-
-    loadGame();
-
-    requestAnimationFrame(update);
-}
-function update()
-{
-    dt2 = Date.now();
-    let dt = (dt2 - dt1) / 1000;
-    dt1 = Date.now();
-
-    tmp.number = tmp.number.mul(tmp.multiplier.pow(1/dt));
-
-    requestAnimationFrame(update);
-}
+};
