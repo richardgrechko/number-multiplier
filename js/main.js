e8cdf9756498b7775a27ca47118c66405bf7ebf4
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-03 19:34:03 +0500

    Update main.js
    Signed-off-by: Richardson736 <162537734+richardgrechko@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -38,6 +38,28 @@ let functions = {
         tmp.number = tmp.number.mul(tmp.multiplier.pow(dt));
 
         requestAnimationFrame(update);
+    },
+    formatNumber: function(n, prec=4, prec1000=0, lim=E(10)) {
+        n = E(n);
+        let e = n;
+        if (n.gte(E(10).tetrate(5))) {
+            let slog = n.slog();
+            e = "10^^" + slog.floor() + ";" + E(10).pow(slog.sub(slog.floor())).toFixed(prec);
+        } else if (n.gte(E(10).pow(E(10).pow(lim)))) {
+            let log = n.log10();
+            e = "10^" + format(log);
+        } else if (n.gte(E(10).pow(lim))) {
+            e = E(10).pow(log.sub(log.floor())).toFixed(prec) + "e" + log.floor();
+        } else if (n.gte(1000)) {
+            e = n.toFixed(prec1000);
+        } else if (n.gte(E(10).pow(-prec))) {
+            e = n.toFixed(prec)
+        } else if (n.gte(0)) {
+            e = 0;
+        } else {
+            e = "-" + formatNumber(n.negate(), prec, prec1000, lim)
+        }
+        return e;
     }
 };
 let onCreate = function()
