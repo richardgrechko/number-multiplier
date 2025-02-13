6a0c75dd463f05e6b86db530294e3790edcae120
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2025-02-13 18:34:52 +0500

    Update main.js
    Signed-off-by: Richardson736 <162537734+richardgrechko@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -1,25 +1,31 @@
 let dt, dt1, dt2;
 function E(n)
 {
-    return new Decimal(n)
+	return new Decimal(n)
 }
 const tmp = {
-    number: E(1),
-    multiplier: E(1.01),
-    infinities: E(0),
-    brokenInfinity: false
+	number: E(1),
+	multiplier: E(1.01),
+	infinities: E(0),
+	brokenInfinity: false,
+	autoInfinity: 1,
+}
+let update = function()
+{
+	tmp.number = tmp.number.mul(tmp.multiplier.pow(0.016));
+	requestAnimationFrame(update());
 }
 let onCreate = function()
 {
-    initialGame = functions.getSaveCode();
+	initialGame = functions.getSaveCode();
 
-    functions.loadGame();
+	functions.loadGame();
 
-    requestAnimationFrame(functions.update);
+	requestAnimationFrame(functions.update);
 }
 var app = new Vue({
-    el: "#app",
-    data: tmp,
-    methods: functions,
-    created: onCreate
+	el: "#app",
+	data: tmp,
+	methods: functions,
+	created: onCreate
 });
