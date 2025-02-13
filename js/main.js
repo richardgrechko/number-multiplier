2b538a318bd11d4403268935f7e941333b67f42a
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2025-02-13 22:01:30 +0500

    Update main.js
    Signed-off-by: Richardson736 <162537734+richardgrechko@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -6,16 +6,24 @@ function E(n)
 const tmp = {
 	points: E(1),
 	multiplier: E(1.01),
+	gain: E(0),
 	infinities: E(0),
 	brokenInfinity: false,
-	autoInfinity: 1,
 }
 let update = function()
 {
 	dt1 = Date.now();
 	dt = dt1-dt2;
 	dt2 = Date.now();
-	tmp.points = tmp.points.mul(tmp.multiplier.pow(dt));
+	tmp.points = tmp.points.mul(tmp.gain.pow(dt));
+	if (tmp.points.gte(Number.MAX_VALUE))
+	{
+		tmp.gain = tmp.multiplier.pow(E(1).div(tmp.points.log10().div(E(2).log10()).sub(1024)))
+	}
+	else
+	{
+		tmp.gain = tmp.multiplier
+	}
 	requestAnimationFrame(update);
 }
 let onCreate = function()
