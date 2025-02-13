597c6d53abc9e3acd9b4632478e2941ff1f23551
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2025-02-13 22:50:30 +0500

    Update upgrade-table.js
    Signed-off-by: Richardson736 <162537734+richardgrechko@users.noreply.github.com>

--- js/components/upgrade-table.js
+++ js/components/upgrade-table.js
@@ -7,6 +7,6 @@ Vue.component("upgrade-table", {
         {
 		totalProduction: () => functions.getMultiplier()
         },
-	template: `<upgrade v-for="(u, i) in upgrades" :upgrade="u" :key="i">
+	template: `<upgrade v-for="(u, i) in upgrades" :upgrade="u" :key="i"></upgrade>
 <div class="big">Your total multiplier is {{formatNumber(totalProduction, 2, 2, 303)}}</div>`
 });
