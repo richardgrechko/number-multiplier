feff7189b797853b0bcea53fbf31127fff8e3669
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2025-02-15 19:44:39 +0500

    Update main.js
    Signed-off-by: Richardson736 <162537734+richardgrechko@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -5,12 +5,12 @@ let update = function()
 	dt = (dt1-dt2)/1000;
 	dt2 = Date.now();
 	game.points = game.points.mul(game.gain.pow(dt));
-	game.multiplier = functions.getMultiplier().mul(1.01).mul(E(2).pow(game.infinities)).mul(E(1024).pow(game.eternities)).mul(E(Number.MAX_VALUE).pow(game.realities))
+	game.multiplier = functions.getMultiplier().mul(1.01).mul(E(2).pow(game.infinities)).mul(E(1023).pow(game.eternities)).mul(E(Number.MAX_VALUE).pow(game.realities))
 	game.softcapRoot = game.points.log10().div(E(2).log10()).sub(1024).div(game.softcapWeaken);
 	if (game.points.gte(Number.MAX_VALUE))
 	{
 		game.pointsAreInfinite = true;
-		game.gain = game.multiplier.pow(E(1).div(game.points.log10().div(E(2).log10()).sub(1024).div(game.softcapWeaken)))
+		game.gain = game.multiplier.pow(E(1).div(game.points.log10().div(E(2).log10()).sub(1023).div(game.softcapWeaken)))
 	}
 	else
 	{
