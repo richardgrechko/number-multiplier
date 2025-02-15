5c4af6154be501a6ac14252610c02abac70cda46
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2025-02-15 18:13:33 +0500

    Update main.js
    Signed-off-by: Richardson736 <162537734+richardgrechko@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function E(n)
 const game = {
 	points: E(1),
 	multiplier: E(1.01),
-	gain: E(0),
+	gain: E(1),
 	upgrades: [
 		new Upgrade("A little increase", E(1.1), E(1.02), E(1.01)),
 		new Upgrade("Increasing!", E(1.5), E(1.2), E(1.1)),
