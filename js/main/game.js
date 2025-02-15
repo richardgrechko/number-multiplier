5b6d630b6df413bd79526481c87933edf0d9616a
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2025-02-15 19:29:19 +0500

    Create game.js
    Signed-off-by: Richardson736 <162537734+richardgrechko@users.noreply.github.com>

---
+++ js/main/game.js
@@ -0,0 +1,22 @@
+const game = {
+	points: E(1),
+	softcapRoot: E(1),
+	softcapWeaken: E(1),
+	pointsAreInfinite: false,
+	multiplier: E(1.01),
+	gain: E(1),
+	upgrades: [
+		new Upgrade("A little increase", E(1.2), E(1.1), E(1.01)),
+		new Upgrade("Increasing!", E(1.5), E(1.2), E(1.1)),
+		new Upgrade("Quickly Growing", E(16), E(4), E(2)),
+		new Upgrade("What?", E(1048576), E(4096), E(128)),
+	],
+	infinities: E(0),
+	brokenInfinity: false,
+	eternities: E(0),
+	realities: E(0),
+	gameWon: false,
+	settings: {
+		tab: "main"
+	}
+}
